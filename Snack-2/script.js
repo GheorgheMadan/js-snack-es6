ffda174d497f3ca4cf9b40052108601f6efb558f
Gheorghe <madangheorghe.30@gmail.com>
2025-01-16 15:46:47 +0100

    start second ex

--- Snack-2/script.js
+++ Snack-2/script.js
@@ -1,24 +1,60 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-// Genero un numero da 1 a 6 per l'utente
-let dadiUtene = Math.floor(Math.random() * 6) +1;
+// creo l'array con le squadre di calcio 
+const teams = [
+    {
+        'nome': 'Inter',
+        'points': 0,
+        'foul': 0
+    },
+    {
+        'nome': 'Fiorentina',
+        'points': 0,
+        'foul': 0
+    },
+    {
+        'nome': 'Atalanta',
+        'points': 0,
+        'foul': 0
+    },
+    {
+        'nome': 'Sampdoria',
+        'points': 0,
+        'foul': 0
+    },
+    {
+        'nome': 'Lazio',
+        'points': 0,
+        'foul': 0
+    },
+    {
+        'nome': 'Roma',
+        'points': 0,
+        'foul': 0
+    }
+]
 
-// // Genero un numero da 1 a 6 per il pc 
-let dadiPc = Math.floor(Math.random() * 6) +1;
+const tableData = document.getElementById('tabella')
 
-// // stampo i risultati
-console.log("Risultati: ") 
-console.log("Player: " + dadiUtene, "Bot: " + dadiPc)
-
-// // SE il punteggio ottenuto dall'utente  è più alto
-if (dadiUtene > dadiPc){
-    // ALLORA stampo l'alert con il punteggio e il vincitore 
-    console.log("Hai vinto!! il tuo punteggio è " + dadiUtene)
-    // ALTRIMENTI hai perso 
-} else if (dadiUtene === dadiPc) {
-    console.log("Pareggio!")
-} else {
-    console.log("Hai perso, ha vinto il PC!")
+for(let k of teams){
+    console.table(teams[k])
+    tableData.innerHTML += `<tr> <td> ${teams[k].nome} </td></tr>`
 }
+
+
+
+
+
+
+
+
+
+
+// FUNZIONE che genera un numero da - a - 
+function generaNumeroRandomRange(min, max){
+    const numeroGenerato = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return numeroGenerato
+}
+
 
 
 

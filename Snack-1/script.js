2f03fd37ae9628add174ae35af3f2c31a3390423
Gheorghe <madangheorghe.30@gmail.com>
2025-01-16 14:54:38 +0100

    fine 1 esercizio

--- Snack-1/script.js
+++ Snack-1/script.js
@@ -27,11 +27,23 @@ const bicycles = [
 ]
 // Stampo in pagina l'array 
 const outputList = document.getElementById("output");
+const outputBicycle = document.getElementById('output-peso-minore')
 
 for(let k in bicycles){
     console.table(bicycles[k]) // stampo in console ogni oggetto
     // stampo in pagina la lista di oggetti
     outputList.innerHTML += `<li>${bicycles[k].nome}: ${bicycles[k].pesoKg} kg</li>` 
+}
+
+let biciLeggera = bicycles[0] //uso una variabile per memorizzare l'oggetto con il peso più basso 
+// uso il ciclo for per confrontare ogni bici dell'array con quella memorizzata nella variabile biciLEGERA
+for(let i = 0; i < bicycles.length; i++){
+    if(bicycles[i].pesoKg < biciLeggera.pesoKg){
+        biciLeggera = bicycles[i]
+        console.log(biciLeggera) //stampo il risultato in console 
+        // stampo il risultato in pagina
+        outputBicycle.innerHTML = `<strong>${biciLeggera.nome}</strong> con il peso di <strong>${biciLeggera.pesoKg}</strong>`
+    }
 }
 
 

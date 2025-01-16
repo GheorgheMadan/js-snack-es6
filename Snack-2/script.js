874634713bd4e96f4da790ee93dd0a5a148c141e
Gheorghe <madangheorghe.30@gmail.com>
2025-01-16 17:10:53 +0100

    fine esercizio

--- Snack-2/script.js
+++ Snack-2/script.js
@@ -34,7 +34,7 @@ const teams = [
 
 const tableData = document.getElementById('tabella')
 
-// con il ciclo for cambio il valore di points 
+// con il ciclo for cambio il valore di points e foul
 for (let i = 0; i < teams.length; i++){
     teams[i].points = generaNumeroRandomRange(1, 114)
     teams[i].foul = generaNumeroRandomRange(1, 114)
@@ -46,18 +46,20 @@ for(let k in teams){
     console.log(teams[k].nome) // stampo in console ogni oggetto
     // stampo in pagina la lista di oggetti
     tableData.innerHTML += `<tr>
-                                <td>${teams[k].nome}</td>
-                                <td>${teams[k].points}</td>
-                                <td>${teams[k].foul}</td> 
+                                <td class="border">${teams[k].nome}</td>
+                                <td class="border">${teams[k].points}</td>
+                                <td class="border">${teams[k].foul}</td> 
                             </tr>` 
 }
 
+// creo un array vuoto
 const namesFouls = []
-
+// con il ciclo for cerco e aggiungo gli elementi che mi servono nel nuovo array
 for (let i = 0; i < teams.length; i++){
-    namesFouls.push(teams[i].nome, teams[i].foul)
-    console.log(namesFouls)
+    let objectTeam = {'nome': teams[i].nome, 'foul': teams[i].foul}
+    namesFouls.push(objectTeam)
 }
+console.table(namesFouls)
 
 
 
